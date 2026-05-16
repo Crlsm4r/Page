b21909fe126dea91925b90b3892ad302f8424a0d
Crlsm4r <141458167+Crlsm4r@users.noreply.github.com>
2026-05-16 11:46:24 -0500

    Update web1.js

--- web1.js
+++ web1.js
@@ -1,42 +1,102 @@
-// BOTON ANIMACION
+// =========================
+// TITULO
+// =========================
 
-const boton = document.getElementById("btnAnimar");
+let titulo = "Nombre";
 
-const cubo = document.querySelector(".cubo");
+let cambio = document.querySelector(".Titulo");
 
-boton.addEventListener("click", () => {
+cambio.innerHTML = titulo;
 
-    cubo.style.animation = "none";
+let tex_ver = cambio.innerText;
 
-    setTimeout(() => {
+if (tex_ver == "Nombre") {
 
-        cubo.style.animation = "rotar 1s infinite linear";
+    cambio.innerHTML = "CV";
 
-    }, 100);
-});
+} else {
 
+    console.log("No se cumple");
+}
+
+// =========================
+// RESEÑA
+// =========================
+
+let carrera = "Mecatrónico";
+
+let rubro = "diseño";
+
+let tipo = "equipo";
+
+let parrafoResena = document.querySelector(".reseña2");
+
+function campar(carrera, rubro, tipo){
+
+    let contenido = `
+    Ingeniero ${carrera}. Capacidad para el manejo de programas de ${rubro} y programación de diferentes campos, acostumbrado al trabajo en ${tipo} manteniendo un perfil de líder capaz de organizar un equipo de trabajo, participación en eventos y concursos. Proactivo, innovador, creativo, responsable con disposición al aprendizaje y adaptabilidad al medio.
+    `;
+
+    return contenido;
+}
+
+parrafoResena.innerHTML = campar(carrera, rubro, tipo);
+
+// =========================
 // FORMULARIO
+// =========================
 
-const formulario = document.getElementById("formulario");
+const form = document.getElementById("form");
 
 const nombre = document.getElementById("nombre");
 
-const alerta = document.getElementById("alerta");
+const alertas = document.getElementById("alertas");
 
-formulario.addEventListener("submit", (e) => {
+// VALIDAR
 
-    e.preventDefault();
+function validarFormulario(){
+
+    let warnings = "";
+
+    let valido = true;
+
+    alertas.innerHTML = "";
+
+    // VALIDAR NOMBRE
 
     if(nombre.value.trim().length < 4){
 
-        alerta.innerHTML = "El nombre debe tener mínimo 4 caracteres";
+        warnings += "El nombre debe contener más de 4 caracteres";
 
-        alerta.style.color = "red";
+        valido = false;
+    }
+
+    // MOSTRAR MENSAJE
+
+    if(!valido){
+
+        alertas.innerHTML = warnings;
+
+        alertas.style.color = "red";
 
     }else{
 
-        alerta.innerHTML = "Formulario enviado correctamente";
+        alertas.innerHTML = "Formulario enviado correctamente";
 
-        alerta.style.color = "cyan";
+        alertas.style.color = "green";
+    }
+
+    return valido;
+}
+
+// EVENTO SUBMIT
+
+form.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+    if(validarFormulario()){
+
+        form.submit();
     }
 });
